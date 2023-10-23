667a470fdc603a3dce7feb19cbfe30b49176b1af
Killian Hedou <killian.hedou@gmail.com>
2023-10-23 16:20:26 -0400

    Added logo image, and initialized on new computer

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,9 +1,20 @@
 import styles from "../pages/repsonsive.module.css";
+import { Link } from "react-router-dom";
+
 function Header() {
     return (
         <div className={styles.header}>
-  <h1>sample</h1>
-</div>
+            <img src="" alt="logo" />
+            <div className={styles.menu}>
+            <ul>
+                <li> <Link to="/"> Home </Link> </li>
+                <li> bla</li>
+                <li>bla</li>
+                <li>smple</li>
+                <li><Link to="/auth"> Login/Register </Link> </li>
+            </ul>
+            </div>
+        </div>
 
     );
 };
