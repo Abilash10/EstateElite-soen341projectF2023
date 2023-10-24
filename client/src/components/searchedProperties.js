b864807de56ad5d747d5030fc41253b95be4eda7
Killian <killian.hedou@gmail.com>
2023-10-24 15:19:13 -0400

    Added search button

---
+++ client/src/components/searchedProperties.js
@@ -0,0 +1,10 @@
+function SearchedProperties() {
+    console.log("SearchedProperties");
+    return (
+        <div>
+            <h1>Searched Properties</h1>
+        </div>
+    );
+}
+
+export default SearchedProperties;
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,4 +1,5 @@
 import styles from "./home.module.css";
+import sP from "../components/searchedProperties.js";
 
 function Home() {
     return (
@@ -9,6 +10,7 @@ function Home() {
     <input className={styles.searchBox} type="text" placeholder="Search by location or address..." name="searchProperties" />
     <div className={styles.buttonContainer}>
       <button className={styles.advancedFiltersBtn} type="submit">Advanced Filters</button>
+      <button className={styles.searchBtn} type="submit" onSubmit={sP}>Search</button>
     </div>
   </div>
 </div>
