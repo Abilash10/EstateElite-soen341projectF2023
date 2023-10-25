04b74b12c28bd29aa40ba7a5eae794d5ab1c2eb8
Nabih21 <126896277+Nabih21@users.noreply.github.com>
2023-10-24 23:54:27 -0400

    Added the Logout button
    Added a Logout button in the Navigation bar, when the user is logged in, the Logout button will appear and once the user clicks on it it will log them out and take the user back to the login page

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,8 +1,20 @@
 import styles from "./header.module.css";
 import { Link } from "react-router-dom";
 import logoImg from "../assets/EElogo.png";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+
 
 function Header() {
+    const [cookies, setCookies] = useCookies(["access_token"]);
+    const navigate = useNavigate();
+    const logout = () => {
+        setCookies("access_token", "");
+        window.localStorage.removeItem("userID");
+        window.localStorage.removeItem("userType");
+        navigate("/auth");
+    };
+
     return (
         <div className={styles.header}>
             <img className={styles.logo} src={logoImg} alt="logo" />
@@ -11,7 +23,9 @@ function Header() {
                 <li> <Link to="/"> Home </Link> </li>
                 <li>Team</li>
                 <li>Contact</li>
-                <li><Link to="/auth"> Login/Register </Link> </li>
+
+                { !cookies.access_token ? 
+                    (<li><Link to="/auth"> Login/Register </Link> </li>) : (<li onClick={logout} >  Logout </li> )}
             </ul>
             </div>
         </div>
