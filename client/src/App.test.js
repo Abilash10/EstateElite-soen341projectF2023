06ab126d51b6db59c13b05d807cbe00ec8d4183f
Pepen0 <115287672+Pepen0@users.noreply.github.com>
2023-11-10 19:19:27 -0500

    Testing homepage on client end

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,39 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Helper function to render the app with router context
+const renderWithRouter = (ui, { route = '/' } = {}) => {
+  window.history.pushState({}, 'Test page', route);
+  return render(ui, { wrapper: Router });
+};
+
+describe('Home Page', () => {
+  test('renders NavBar with Home link', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByText(/home/i); // This is case-insensitive
+    expect(homeLink).toBeInTheDocument();
+  });
+
+  test('renders search bar', () => {
+    renderWithRouter(<App />);
+    const searchBar = screen.getByPlaceholderText(/search by location or address.../i);
+    expect(searchBar).toBeInTheDocument();
+  });
+
+  test('renders Advanced Filters button', () => {
+    renderWithRouter(<App />);
+    const advancedFiltersButton = screen.getByText(/advanced filters/i);
+    expect(advancedFiltersButton).toBeInTheDocument();
+  });
+
+  test('renders Search button', () => {
+    renderWithRouter(<App />);
+    const searchButton = screen.getByRole('button', { name: /search/i });
+    expect(searchButton).toBeInTheDocument();
+  });
+
+  // Add more tests for other home page elements (Broker list, Team, Contact, Login/Register )
 });
+
