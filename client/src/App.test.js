93c631354faa7a49783e6c09c8244094920c1393
Pepen0 <115287672+Pepen0@users.noreply.github.com>
2023-11-10 22:44:05 -0500

    App router issue fix

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,22 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-
-// Helper function to wrap components with Router since we use 'useRouteMatch' hook in components
-const renderWithRouter = (ui, { route = '/' } = {}) => {
-  window.history.pushState({}, 'Test page', route);
-  return render(ui, { wrapper: Router });
-};
 
 describe('App Component', () => {
   test('renders the navigation bar and its links', () => {
-    renderWithRouter(<App />);
+    render(<App />);
     expect(screen.getByText('Home')).toBeInTheDocument();
     expect(screen.getByText('My Properties')).toBeInTheDocument();
     expect(screen.getByText('Team')).toBeInTheDocument();
     expect(screen.getByText('Contact')).toBeInTheDocument();
     expect(screen.getByText('Login/Register')).toBeInTheDocument();
   });
+
 });
 
