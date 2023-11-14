14be6ef18aac78cf9d6b68b80f627610919973fd
Pepen0 <115287672+Pepen0@users.noreply.github.com>
2023-11-13 20:24:36 -0500

    Test 15
    Looking to see if the issue comes from the code or the pipeline

--- Test/UAT_2_4_test.py
+++ Test/UAT_2_4_test.py
@@ -5,69 +5,73 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import logging
 
-# Initialize logging
-logging.basicConfig(level=logging.INFO)
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
-# Run Chrome in headless mode for the testing environment
-chrome_options = Options()
-chrome_options.add_argument("--headless")
+# Setup Chrome options
+options = Options()
+options.headless = True  # Run in headless mode
 
-# Initialize the WebDriver
-driver = webdriver.Chrome(options=chrome_options)
-
-wait = WebDriverWait(driver, 10)
+# Initialize the Chrome driver
+driver = webdriver.Chrome(options=options)
+wait = WebDriverWait(driver, 10)  # Setup wait variable
 
 try:
-    # Navigate to the homepage
-    driver.get("http://localhost:3000/")
-    logging.info("Navigated to the homepage.")
+    # Navigate to the URL
+    driver.get("http://localhost:3000")
+    logging.info("Navigated to the homepage")
 
-    # Click the Login button
-    login_button = wait.until(
-        EC.presence_of_element_located(
-            (By.XPATH, "//a[normalize-space()='Login/Register']")
+    Click_Broker = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//a[normalize-space()='Broker List']"))
+    )
+    Click_Broker.click()
+    logging.info("Clicked on broker page")
+
+    # Search broker
+    search_input = wait.until(
+        EC.element_to_be_clickable(
+            (
+                By.XPATH,
+                "/html[1]/body[1]/div[1]/div[1]/div[2]/div[1]/input[1]",
+            )  # might cause a bug later on!Abs xpath!
         )
     )
-    login_button.click()
-    logging.info("Clicked the Login button.")
+    search_input.clear()
+    search_input.send_keys("Tester")
+    logging.info("Search criteria entered")
 
-    # Wait and fill in the username and password
-    username_field = wait.until(EC.presence_of_element_located((By.ID, "username")))
-    username_field.send_keys("existing_username")
-    logging.info("Filled in the username.")
-
-    password_field = wait.until(EC.presence_of_element_located((By.ID, "password")))
-    password_field.send_keys("existing_password")
-    logging.info("Filled in the password.")
-
-    # Click the Login button
-    wait.until(
+    # Click the search button
+    search_button = wait.until(
         EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']"))
-    ).click()
-    logging.info("Clicked the final Login button.")
+    )
+    search_button.click()
+    logging.info("Clicked Search")
 
-    # Check for error message if the username or password is incorrect
-    try:
-        error_message = wait.until(
-            EC.presence_of_element_located((By.XPATH, "//div[@class='error-message']"))
-        )
-        assert False, f"Error message displayed: {error_message.text}"
-    except TimeoutException:
-        logging.info("No error message displayed.")
-
-    # Verify the user is on the homepage by checking the search bar's presence
-    search_bar = wait.until(
-        EC.presence_of_element_located(
-            (By.XPATH, "//input[@placeholder='Search by location or address...']")
+    # Wait for the Broker to be visible
+    Brokers = wait.until(
+        EC.visibility_of_all_elements_located(
+            (By.CLASS_NAME, "searchedProperties_propertyCard__bIMx-")
         )
     )
-    assert search_bar.is_displayed(), "Search bar is not displayed, not on homepage."
-    logging.info("User is on the homepage, search bar is displayed.")
+    logging.info("Brokers are visible now")
+
+    # Display broker properly
+    first_Broker = Brokers[0]
+    Broker_id = first_Broker.get_attribute("id")
+
+    if Broker_id:
+        logging.info(f"First broker found: {Broker_id}")
+        logging.info(f"\n{first_Broker.text}")
+    else:
+        logging.error(f"Broker was not found")
+
 
 except Exception as e:
     logging.error(f"An error occurred: {e}")
-
+    raise
 finally:
-    # Close the WebDriver
+    # Close the browser
     driver.quit()
-    logging.info("WebDriver closed.")
+    logging.info("Browser closed")
